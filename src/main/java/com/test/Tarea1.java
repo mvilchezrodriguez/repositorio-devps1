19290bc3deafe34ad9d74adb14a4cb86727ad017
MiguelVR <vilchezrodriguezm@gmail.com>
2018-09-09 20:05:02 -0500

    tercer commit tarea 1

--- src/main/java/com/test/Tarea1.java
+++ src/main/java/com/test/Tarea1.java
@@ -5,6 +5,8 @@ public class Tarea1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("texto de prueba 1");
+      System.out.println("texto de prueba 2");
+      System.out.println("texto de prueba 3"); 
 	} 
  
 }
